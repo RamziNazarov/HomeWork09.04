47c39694a1a59cc5965d1ec288fc36af5c3f17bf
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-09 20:10:07 +0300

    create project and file DocumentWorker and classes DocumentWorker, ProDocumentWorker, ExpertDocumentWorker in file DocumentWorker

---
+++ DocumentWorker.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HomeWork09._04
+{
+    class DocumentWorker
+    {
+
+    }
+    class ProDocumentWorker : DocumentWorker
+    {
+
+    }
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace HomeWork09._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = File.ReadAllLines("text.txt");
+            for(int i = 0; i < array.Length; i++)
+            {
+                System.Console.WriteLine(array[i]);
+                array[i] += array[i];
+            }
+            string txt = "asd";
+            string type = ".txt";
+            string tt = txt + type;
+            File.WriteAllLines(tt,array);
+
+            Console.ReadKey();
+        }
+    }
+}
