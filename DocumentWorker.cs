655c90213b854928cc931837a7f618e35ad0b397
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-09 21:26:38 +0300

    ended project

--- DocumentWorker.cs
+++ DocumentWorker.cs
@@ -4,14 +4,41 @@ namespace HomeWork09._04
 {
     class DocumentWorker
     {
-
+        public virtual void OpenDocument()
+        {
+            System.Console.WriteLine("Документ открыт");
+        }
+        public virtual void EditDocument()
+        {
+            System.Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+        public virtual void SaveDocument()
+        {
+            System.Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
     }
     class ProDocumentWorker : DocumentWorker
     {
-
+        public override void EditDocument()
+        {
+            System.Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            System.Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
     }
     class ExpertDocumentWorker : ProDocumentWorker
     {
-        
+        public override void SaveDocument()
+        {
+            System.Console.WriteLine("Сохранить документ в новом формате?\nДа - чтобы сохранить в новом\nНет - чтобы сохранить в старом\nИначе файл не будет сохранен");
+            string chose = Console.ReadLine();
+            if(chose.ToLower() == "да")
+            System.Console.WriteLine("Документ сохранен в новом формате");
+            else if(chose.ToLower() == "нет")
+            System.Console.WriteLine("Документ был сохранен в старом формате");
+            else System.Console.WriteLine("Документ не был сохранен");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,18 +7,30 @@ namespace HomeWork09._04
     {
         static void Main(string[] args)
         {
-            string[] array = File.ReadAllLines("text.txt");
-            for(int i = 0; i < array.Length; i++)
+            Console.WriteLine("Функционал программы\nВ обычной версии: Открытые документа\nВ версии Про: Открытые документа - Изменение документа - Сохранение документа в старом формате\nВ весрии Эксперт: Открытые документа - Изменение документа - Сохранение документа в других форматах");
+            System.Console.Write("Для обычной версии ключ не нужен\nКлюч для Про версии: 1q2w-3e4r-5t6y-7u8i\nКлюч для Эксперт версии: 1qaz-2wsx-3edc-4rfv\nВведите ключ: ");
+            string key = Console.ReadLine();
+            DocumentWorker DW = (key == "1q2w-3e4r-5t6y-7u8i")? new ProDocumentWorker():(key == "1qaz-2wsx-3edc-4rfv")?new ExpertDocumentWorker():new DocumentWorker();
+            int choose = 0;
+            while(choose != 4)
             {
-                System.Console.WriteLine(array[i]);
-                array[i] += array[i];
+                Console.Clear();
+                System.Console.Write("1. Открыть документ\n2. Отредактировать документ\n3. Сохранить документ\n4. Выход\nВаш выбор: ");
+                choose = int.Parse(Console.ReadLine());
+                switch(choose)
+                    {
+                        case 1:
+                            DW.OpenDocument();
+                        break;
+                        case 2:
+                            DW.EditDocument();
+                        break;
+                        case 3: 
+                            DW.SaveDocument();
+                        break;
+                    }
+                    System.Console.Write("Нажмите на любую клавишу чтобы продолжить...");Console.ReadKey();
             }
-            string txt = "asd";
-            string type = ".txt";
-            string tt = txt + type;
-            File.WriteAllLines(tt,array);
-
-            Console.ReadKey();
         }
     }
 }
